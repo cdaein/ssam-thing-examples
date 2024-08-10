cbbc340ed0db8e86786d29768079d5e2a2a9ca04
MBP14 <3522003+cdaein@users.noreply.github.com>
2024-08-09 17:38:10 -0700

    feat: color-gradients

---
+++ src/color-gradients.ts
@@ -0,0 +1,53 @@
+// oklab gradient
+
+import { Oklab, hsv, mix, oklab, rgb } from "@thi.ng/color";
+import { Sketch, SketchSettings, ssam } from "ssam";
+import * as g from "@thi.ng/geom";
+import { draw } from "@thi.ng/hiccup-canvas";
+
+const sketch: Sketch<"2d"> = ({ wrap, context: ctx, width, height }) => {
+  // create two colors
+  // need conversion from HSV -> OKLAB
+  const colA = oklab(rgb(hsv(0, 1, 1)));
+  const colB = oklab(rgb(hsv([0.6, 1, 1])));
+  const colorStops: Oklab[] = [];
+
+  const numStops = 36;
+
+  // generate oklab gradient colors
+  for (let i = 0; i < numStops; i++) {
+    const mid = mix(oklab(), colA, colB, i / numStops) as Oklab;
+    colorStops.push(mid);
+  }
+
+  // shapes
+  const margin = 0.06;
+  const rects = colorStops.map((col, i) =>
+    g.rectWithCentroid(
+      [
+        width * (margin + (i * (1 - margin * 2)) / (numStops - 1)),
+        height * 0.5,
+      ],
+      [width * ((1 - margin * 2) / (numStops - 1) - 0.01), height * 0.5],
+      {
+        fill: col,
+      },
+    ),
+  );
+
+  wrap.render = ({ width, height }) => {
+    ctx.fillStyle = `gray`;
+    ctx.fillRect(0, 0, width, height);
+
+    draw(ctx, g.group({}, rects));
+  };
+};
+
+const settings: SketchSettings = {
+  dimensions: [800, 800],
+  pixelRatio: window.devicePixelRatio,
+  animate: false,
+  filename: import.meta.url?.split("/").pop()?.split(".")[0] || undefined,
+};
+
+ssam(sketch, settings);
